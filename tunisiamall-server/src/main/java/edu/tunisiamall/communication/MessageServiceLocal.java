2f4db3e124861a905638945c147e66f8c31b38b3
Elyes <elyes.benabdelkader@esprit.tn>
2016-02-10 09:35:16 +0100

    update

--- tunisiamall-server/src/main/java/edu/tunisiamall/communication/MessageServiceLocal.java
+++ tunisiamall-server/src/main/java/edu/tunisiamall/communication/MessageServiceLocal.java
@@ -1,8 +1,16 @@
 package edu.tunisiamall.communication;
 
+import java.util.List;
+
 import javax.ejb.Local;
+
+import edu.tunisiamall.entities.Message;
+import edu.tunisiamall.entities.User;
 
 @Local
 public interface MessageServiceLocal {
-
+	public List<Message> getMessages(User u);
+	public void sendMessage(User src, User dest, String text);
+	public void deleteMessage(Message m);
+	public void setMessageSeen(Message m);
 }
--- tunisiamall-server/src/main/java/edu/tunisiamall/communication/MessageServiceRemote.java
+++ tunisiamall-server/src/main/java/edu/tunisiamall/communication/MessageServiceRemote.java
@@ -1,8 +1,14 @@
 package edu.tunisiamall.communication;
 
+import java.util.List;
 import javax.ejb.Remote;
+import edu.tunisiamall.entities.User;
+import edu.tunisiamall.entities.Message;
 
 @Remote
 public interface MessageServiceRemote {
-
+	public List<Message> getMessages(User u);
+	public void sendMessage(User src, User dest, String text);
+	public void deleteMessage(Message m);
+	public void setMessageSeen(Message m);
 }
