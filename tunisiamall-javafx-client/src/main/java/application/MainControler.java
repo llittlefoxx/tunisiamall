9ffced34773a044a4c4b6519375b300071f143aa
Elyes <elyes.benabdelkader@esprit.tn>
2016-02-16 21:06:10 +0100

    azeazedsf

--- tunisiamall-javafx-client/src/main/java/application/MainControler.java
+++ tunisiamall-javafx-client/src/main/java/application/MainControler.java
@@ -2,7 +2,9 @@ package application;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 public class MainControler {
 
@@ -29,11 +31,62 @@ public class MainControler {
 
     @FXML
     private Button categoriesButton;
+    
+    // Panes
+    public static Pane shopPane;
+    public static Pane usersPane;
+    public static Pane biPane;
+    public static Pane eventsPane;
+    public static Pane categoriesPane;
+    public static Pane messagingPane;
+    public static Pane guestbookPane;
 
 
     @FXML
     void initialize() {
     	
+    }
+    
+    @FXML
+    void shopTab(MouseEvent event) {
+    	homePane.getChildren().clear();
+    	homePane.getChildren().add(shopPane);
+    }
+
+    @FXML
+    void usersTab(MouseEvent event) {
+    	homePane.getChildren().clear();
+    	homePane.getChildren().add(shopPane);
+    }
+
+    @FXML
+    void biTab(MouseEvent event) {
+    	homePane.getChildren().clear();
+    	homePane.getChildren().add(shopPane);
+    }
+
+    @FXML
+    void eventsTab(MouseEvent event) {
+    	homePane.getChildren().clear();
+    	homePane.getChildren().add(shopPane);
+    }
+
+    @FXML
+    void categoriesTab(MouseEvent event) {
+    	homePane.getChildren().clear();
+    	homePane.getChildren().add(shopPane);
+    }
+
+    @FXML
+    void messagingTab(MouseEvent event) {
+    	homePane.getChildren().clear();
+    	homePane.getChildren().add(shopPane);
+    }
+
+    @FXML
+    void guestbookTab(MouseEvent event) {
+    	homePane.getChildren().clear();
+    	homePane.getChildren().add(shopPane);
     }
 }
 
