881cbb4fd86cccfa0b559b1efaa85b1b65e02254
llittlefoxx <mohamedali.tlili@esprit.tn>
2016-03-15 21:30:58 +0100

    my final commit

--- tunisiamall.server/tunisiamall.server-ejb/src/main/java/edu/tunisiamall/biservices/IndicatorsServiceLocal.java
+++ tunisiamall.server/tunisiamall.server-ejb/src/main/java/edu/tunisiamall/biservices/IndicatorsServiceLocal.java
@@ -1,12 +1,18 @@
 package edu.tunisiamall.biservices;
 
+import java.util.HashMap;
 import java.util.List;
 
 import javax.ejb.Local;
 
 import edu.tunisiamall.entities.Product;
+import edu.tunisiamall.entities.Promotion;
+import edu.tunisiamall.entities.Store;
 
 @Local
 public interface IndicatorsServiceLocal {
 	public List<Product> getAllProducts();
+	public HashMap<Product, Integer> getHotProducts();
+	public List<Product> findProductByLib(String lib);
+	public HashMap<Store, Promotion> getPromotionByStore();
 }
--- tunisiamall.server/tunisiamall.server-web/src/main/java/web/service/main/ProductServices.java
+++ tunisiamall.server/tunisiamall.server-web/src/main/java/web/service/main/ProductServices.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.jws.WebParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import edu.tunisiamall.biservices.IndicatorsServiceLocal;
@@ -37,5 +40,23 @@ public class ProductServices {
 			Custumised.add(cp);
 		}
 		return Custumised;
+	}
+	
+	@GET
+	@Produces("application/json")
+	@Path("/rech/{name}")
+	public List<CustomizedProduct> findProductByLibelle(@PathParam(value="name")String name){
+		List<Product> origin = new ArrayList<Product>();
+		List<CustomizedProduct> custumised = new ArrayList<CustomizedProduct>();
+		origin = indicatorsServiceLocal.findProductByLib(name);
+		for (Product product : origin) {
+			CustomizedProduct cp = new CustomizedProduct(product.getIdProduct(), product.getQte(), product.getLibelle(),
+					product.getSubcategory().getCategory().getIdCategory(),
+					product.getSubcategory().getCategory().getLibelle(), product.getPromotion().getValue(),
+					product.getSubcategory().getIdSubCategory(), product.getSubcategory().getLibelle(), product.getImages(),
+					product.getSellPrice());
+			custumised.add(cp);
+		}
+		return custumised;
 	}
 }
